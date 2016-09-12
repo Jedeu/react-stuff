d665d5a0a460a88d4775fdad5da2202d802ba7ba
Jedeu <jed.piezas@gmail.com>
2016-09-11 20:20:24 -0700

    Added winston and morgan. Morgan's logs now stream to winston transport

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+import {logger} from './util';
 
 const app = express();
+
+app.use(morgan('combined', {stream: logger.stream}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,7 +16,7 @@ app.get('/', (req, res) => {
 });
 
 app.use((err, req, res) => {
-  console.error(err.stack);
+  logger.error(err.stack);
   res.status(500).send(err);
 });
 
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,13 @@
 import app from './app';
+import {logger} from './util';
 
 app.listen(8080, function() {
   const host = this.address().address;
   const port = this.address().port;
-  console.log(`Shard listening at http://${host}:${port}`);
+  logger.info(`Experts-server listening at http://${host}:${port}`);
 });
+
+process.on('uncaughtException', err => logger.error('uncaught exception', err));
+
+process.on('unhandledRejection', error => logger.error('unhandled rejection', error));
+
