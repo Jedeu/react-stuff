292629629511d70c13e4eaed9824f9ba22ba60a6
Jedeu <jed.piezas@gmail.com>
2016-09-14 20:49:27 -0700

    Tests now use require hook. Also actually pass

--- server/test/index.js
+++ server/test/index.js
@@ -1,19 +1,4 @@
-import test from 'tape';
-import request from 'supertest';
+require ('babel-core/register');
 
-import app from '../src/app';
+require('./main');
 
-test('GET /', (t) => {
-  request(app)
-    .get('/')
-    .expect(200)
-    .expect('Content-Type', /text\/html/)
-    .end((err, res) => {
-      const expectedBody = 'Hello World!';
-      const actualBody = res.text;
-
-      t.error(err, 'No error');
-      t.equal(actualBody, expectedBody, 'Retrieve body');
-      t.end();
-    });
-});
